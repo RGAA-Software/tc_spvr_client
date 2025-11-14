96c142047385e11afcd6e4475a3bb0a22cfbb253
Hector <hollow.ichigo.yoo@gmail.com>
2025-11-14 13:26:18 +0800

    add force software
    Signed-off-by: Hector <hollow.ichigo.yoo@gmail.com>

--- spvr_stream.h
+++ spvr_stream.h
@@ -104,6 +104,9 @@ namespace spvr
         // force relay
         bool force_relay_ = false;
 
+        // force software
+        bool force_software_ = false;
+
 
         // Extra
         ///// NOT in database
